5e0cb89ecf6d38639d6ab872f1a28ee6466c399c
Georgios.D <georgios_97@hotmail.com>
2023-03-27 11:42:22 +0200

    Added API to project

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { accessUrl } from "../config";
 
 export default function Login() {
-    return <div>Login</div>;
+    return (
+        <div className="py-10 text-center">
+            <h1 className="text-4xl front-bold mb-1">
+                Welcome to my Spotify clone
+            </h1>
+            <p className="text-text-dimmed mb-5">
+                Please sign in to use the app
+            </p>
+            <a
+                href={accessUrl}
+                className="py-1.5 px-5 bg-primary rounded-md text-xl font-bold"
+            >
+                Sign in
+            </a>
+        </div>
+    );
 }
